59dc799a4ea25f2a3c058eb21110bc807920e90d
pencilbsp <pencil.bsp@gmail.com>
2023-12-17 09:42:51 +0700

    Fix error getting iq.com episode list

--- server/src/libs/extractor/iq.js
+++ server/src/libs/extractor/iq.js
@@ -2,6 +2,24 @@ import { USER_AGENT } from "../configs"
 import { getNextScript } from "../html"
 import { timeToSeconds } from "../format"
 
+async function getEpisodeListSource(albumId, start, end) {
+  const query = new URLSearchParams({
+    platformId: 3,
+    modeCode: "vn",
+    langCode: "en_us",
+    deviceId: "",
+    endOrder: end,
+    startOrder: start,
+  }).toString()
+
+  const headers = new Headers({ "User-Agent": USER_AGENT })
+  // ttps://pcw-api.iq.com/api/v2/episodeListSource/4886493532106801?platformId=3&modeCode=vn&langCode=en_us&deviceId=ac895c382d771fde2c9efe36ba145d4c&endOrder=850&startOrder=801
+  const apiUrl = `https://pcw-api.iq.com/api/v2/episodeListSource${albumId}?${query}`
+  const response = await fetch(apiUrl, { headers })
+  const data = await response.json()
+  console.log(data)
+}
+
 export default async function iqVideoParse(url, options) {
   const response = await fetch(url, { headers: { "user-agent": USER_AGENT } })
   const body = await response.text()
@@ -14,7 +32,7 @@ export default async function iqVideoParse(url, options) {
   const videoType = initialState.play.curVideoInfo.videoType
 
   if (albumId === 0 && videoType === "singleVideo") {
-    const video = initialState.play.curVideoInfo
+    const video = initialState.play.cachePlayList
     const requireVip = video.vipInfo.isVip === 1 || video.payMark === "lock"
 
     return [
@@ -32,8 +50,19 @@ export default async function iqVideoParse(url, options) {
     ]
   }
 
-  const videoList = initialState.play.cachePlayList["1"]
-  if (!Array.isArray(videoList)) throw new Error("Không tìm thấy danh sách video")
+  const page = Object.keys(initialState.play.cachePlayList)
+  if (page.length === 0) throw new Error("Không tìm thấy danh sách video")
+
+  const currentPage = initialState.play.curVideoPageInfo.pageNo
+  const videoList = initialState.play.cachePlayList[currentPage]
+
+  // const maxOrder = initialState.play.albumInfo.maxOrder
+  // const totalPageRange = initialState.play.albumInfo.totalPageRange
+  // if (currentPage > 1) {
+  //   const startOrder =
+  //   // const endOrder = Math.min(...videoList.map((v) => v.order))
+  //   // const moreVideos = await getEpisodeListSource(albumId, 0)
+  // }
 
   return [
     videoList
